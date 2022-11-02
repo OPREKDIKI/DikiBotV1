892fad5875032c2ed152aeebf917d8f321f61463
DikiBotz <99316469+OPREKDIKI@users.noreply.github.com>
2022-11-02 19:56:33 +0700

    Create cheat.js

---
+++ plugins/cheat.js
@@ -0,0 +1,25 @@
+let { MessageType } = require('@adiwajshing/baileys')
+
+let handler = async (m, { conn }) => {
+    let user = global.db.data.users[m.sender]
+        if (!args[0]) return m.reply('Masukan Item Yang Mau Di Cheat !')
+        conn.reply(m.chat, `*Succes !*`, m)
+        global.db.data.users[m.sender].${args[0]} = Infinity
+        
+}
+handler.help = ['cheatowner']
+handler.tags = ['owner,premium']
+handler.command = /^(cheat)$/i
+handler.owner = false
+handler.mods = true
+handler.premium = true
+handler.group = false
+handler.private = false
+
+handler.admin = false
+handler.botAdmin = false
+
+handler.fail = null
+handler.money = 0
+
+module.exports = handler
