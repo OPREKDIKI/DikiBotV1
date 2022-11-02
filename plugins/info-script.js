739915ddd95807ac1d4e5af81e7e52122b6bd7a4
OPREKDIKI <dikirafaelsimarmata@gmail.com>
2022-11-02 04:37:00 +0000

    DikiBotV1

---
+++ plugins/info-script.js
@@ -0,0 +1,24 @@
+import fetch from 'node-fetch'
+let handler = async(m, { conn, text, usedPrefix, command }) => {
+let pp = await conn.profilePictureUrl(m.chat).catch(_ => null)
+
+let str = `*Chat Owner Untuk Scriptnya🗿*
+wa.me/6281260730830`
+let wibu = `https://api-reysekha.herokuapp.com/api/random/cosplay?apikey=apirey` 
+let thumb = await(await fetch(wibu)).buffer()
+conn.sendButtonDoc(m.chat, str, wm,'Thankyou','Bilek', m, { contextInfo: { externalAdReply: { showAdAttribution: true,
+    mediaUrl: "https://chat.whatsapp.com/KbdXQZOfyFd0otZw7KooO8",
+    mediaType: "VIDEO",
+    description: "https://www.instagram.com/p/CevoCg5hG-p/?utm_source=ig_web_copy_link", 
+    title: 'Akio Bot MultiDevice',
+    body: wm,
+    thumbnail: thumb,
+    sourceUrl: sgc
+  }
+  } }) 
+          }
+handler.help = ['source code']
+handler.tags = ['info']
+handler.command =  /^(script|sc)$/i
+
+export default handler
